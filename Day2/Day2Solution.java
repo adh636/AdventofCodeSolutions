948056cdc2900694e0cb0cadd551947a6ea601b5
Adam <adh636@gmail.com>
2016-03-30 23:55:58 -0400

    add methods to print solutions and add comments

--- Day2/Day2Solution.java
+++ Day2/Day2Solution.java
@@ -2,22 +2,36 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class Day2Solution {
+public class Day2 {
 
 	public static void main(String[] args) throws FileNotFoundException {
+		
+		printWrappingPaper(new File("Day2Input.txt")); // Print part 1 answer
+		printRibbon(new File("Day2Input.txt")); // Print part 2 answer
+	}
+	
+	public static void printWrappingPaper(File input) throws FileNotFoundException {
+
 		Scanner in = new Scanner(new File("Day2Input.txt"));
-		int totalWrappingPaper = 0;
-		int totalRibbon = 0;
-		
-		while (in.hasNextLine()) {
-			Dimension current = new Dimension(in.nextLine());		
-			wrappingPaper += current.getSurfaceArea() + current.getSlack();	
-			ribbon += current.getRibbon();
+		int wrappingPaper = 0;
+		while (in.hasNextLine()) {	
+			Dimension current = new Dimension(in.nextLine());			
+			wrappingPaper += current.wrappingPaper();
 		}
-		
-		System.out.println("Total wrapping paper: " + totalWrappingPaper);
-		System.out.println("Total ribbon: " + totalRibbon);
 		in.close();
+		System.out.println("Total wrapping paper: " + wrappingPaper);
+	}
+	
+	public static void printRibbon(File input) throws FileNotFoundException {
+		
+		Scanner in = new Scanner(new File("Day2Input.txt")); 
+		int ribbon = 0;
+		while (in.hasNextLine()) {
+			Dimension current = new Dimension(in.nextLine());
+			ribbon += current.ribbon();
+		}
+		in.close();
+		System.out.println("Total ribbon: " + ribbon);
 	}
 }
 
@@ -33,11 +47,13 @@ class Dimension {
 		h = Integer.parseInt(dimensions[2]);
 	}
 	
-	int getSurfaceArea() {
+	// surface area of box
+	int surfaceArea() {
 		return (2 * l * w) + (2 * w * h) + (2 * h * l);
 	}
 	
-	int getSlack() {
+	// extra paper needed beyond surface area
+	int slack() {
 		if (l <= w && h <= w) {
 			return l * h;
 		}
@@ -49,7 +65,13 @@ class Dimension {
 		}
 	}
 	
-	int getRibbon() {
+	// total paper needed to wrap box
+	int wrappingPaper() {
+		return surfaceArea() + slack();
+	}
+	
+	// total ribbon needed for box
+	int ribbon() {
 		if (l <= w && h <= w) {
 			return (2 * l) + (2 * h) + (l * w * h);
 		}
